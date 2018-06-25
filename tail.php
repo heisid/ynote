f42772e6f5047fe8e7a1dfa46b5db0b791b55af3
heisid <weird_alien@protonmail.com>
2018-06-25 09:32:25 +0700

    commenting script for giving active class to navbar. Not that important

--- tail.php
+++ tail.php
@@ -4,11 +4,14 @@
 <script src="assets/js/popper.min.js"></script>
 <!-- Bootstrap Js -->
 <script src="assets/js/bootstrap.min.js"></script>
+<!-- This script basically just give active class to navbar when we're in the corresponing URI -->
+<!--
 <script>
     $(function () {
         $('nav a[href^="/ynote/' + location.pathname.split("/")[2] + '"]').addClass('active');
     });
 </script>
+-->
 </body>
 
 </html>

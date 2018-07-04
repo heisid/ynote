840846cb7d230c70dec4dc5e5facf3d7ddbd8bf1
heisid <weird_alien@protonmail.com>
2018-07-04 11:23:45 +0700

    simple tagging

--- readpost.php
+++ readpost.php
@@ -4,18 +4,22 @@
         $id_post = (int) $_GET['id-post'];
     }
 
-    $sql = "SELECT * FROM posts WHERE id_post = '$id_post'";
-    $result_sql = mysqli_query($db_handle, $sql);
-    $res_sql_array = mysqli_fetch_array($result_sql);
+    $sql_post = "SELECT * FROM posts WHERE id_post = '$id_post'";
+    $sql_tags = "SELECT tag FROM tags
+                INNER JOIN posts ON tags.id_post = posts.id_post 
+                WHERE posts.id_post = '$id_post'";
+    $result_sql_post = mysqli_query($db_handle, $sql_post);
+    $result_sql_tags = mysqli_query($db_handle, $sql_tags);
+    $res_sql_post_array = mysqli_fetch_array($result_sql_post);
 
-    $title_post = $res_sql_array['title_post'];
-    $date_post = $res_sql_array['date_post'];
+    $title_post = $res_sql_post_array['title_post'];
+    $date_post = $res_sql_post_array['date_post'];
 
-    if (!is_null($res_sql_array['date_modified'])) {
-        $date_modified = $res_sql_array['date_modified'];
+    if (!is_null($res_sql_post_array['date_modified'])) {
+        $date_modified = $res_sql_post_array['date_modified'];
     }
 
-    $content = $res_sql_array['content'];
+    $content = $res_sql_post_array['content'];
 
     $style = "readpost.css";
     $page_title = $title_post;
@@ -28,6 +32,12 @@
         echo "<p><em>(Last Modified $date_modified)</em></p>";
     }
     echo "<hr>";
+    
+    echo "<p>Tags: ";
+    while ($tag = mysqli_fetch_array($result_sql_tags)) {
+        echo $tag["tag"].", ";
+    }
+    echo "</p>";
 
     echo $content;
     echo "<hr>";

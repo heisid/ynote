36b4d119e530666a773ac679a4de954f56d240f5
heisid <weird_alien@protonmail.com>
2018-07-06 10:05:40 +0700

    messing up

--- tags.php
+++ tags.php
@@ -4,10 +4,21 @@
     include "navbar.php";
     include_once "config.php";
 
-    $sql = "SELECT DISTINCT tag FROM tags";
-    $result_sql = mysqli_query($db_handle, $sql);
+    if (isset($_GET['tag'])) {
+        $tag = $_GET['tag'];
+        $sql = "SELECT id_post FROM tags where tag = '$tag' ";
+        $result_sql = mysqli_query($db_handle, $sql);
+    } else {
+        $sql = "SELECT DISTINCT tag FROM tags";
+        $result_sql = mysqli_query($db_handle, $sql);
+    }
 
+    if (isset($tag)) {
+        echo "<h1>Post With Tag ".$tag." </h1>";
+    } else {
     echo "<h1>Browse by Tags</h1>";
+    }
+    
     echo "<ul>";
     while($row = mysqli_fetch_array($result_sql)) {
         echo "<li><a href='#'>".$row['tag']."</a></li>";

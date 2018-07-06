cccf6c06b7a8732ad3925f26024f38c18a42592d
Sid <weird_alien@protonmail.com>
2018-07-06 13:07:54 +0700

    insert tag, halfway

--- process.php
+++ process.php
@@ -9,17 +9,24 @@ function redirect() {
 if (isset($_POST["post-submit"])) {
     $post_title = mysqli_real_escape_string($db_handle, $_POST["post-title"]);
     $post_content = mysqli_real_escape_string($db_handle, $_POST["post-content"]);
+    $tags = mysqli_real_escape_string($db_handle, $_POST["tags"]);
+    $tags_array = explode(",", $tags);
     
     if ($_POST["id-post"]) {
         $id_post = $_POST["id-post"];
         $sql = "UPDATE posts SET date_modified=NOW(), title_post='$post_title', content='$post_content'
-        WHERE id_post='$id_post'";
+        WHERE id_post='$id_post';";
+        $sql .= "DELETE FROM tags WHERE id_post='$id_post'";
+        foreach ($tags_array as $tag) {
+            $sql .= "INSERT INTO tags (id_post, tag) VALUE ('$id_post', '$tag');";
+        }
     } else {
         $sql = "INSERT INTO posts (id_post, date_post, date_modified, title_post, content)
         VALUE (NULL, NOW(), NULL, '$post_title', '$post_content')";
     }
 
-    if (mysqli_query($db_handle, $sql)) {
+
+    if (mysqli_multi_query($db_handle, $sql)) {
         redirect();
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($db_handle);
